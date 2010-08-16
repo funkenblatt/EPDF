5778c2da846cf361cf76200d24dde8235988d04d
Jason Feng <jfeng1985@gmail.com>
2010-08-16 11:40:58 -0700

    Functions for name tree lookup.

--- pdf-util.el
+++ pdf-util.el
@@ -35,3 +35,50 @@
        (pdf-dump "\n")))
    (pdf-dref (pdf-doc-catalog doc) '/Outlines)))
 
+(defun pdf-lookup-dest (doc dest)
+  (when (not (pdf-doc-names doc))
+    (setf (pdf-doc-names doc) (make-hash-table :test 'equal)))
+  (let ((out (gethash dest (pdf-doc-names doc))))
+    (if out out
+      (setq out
+	    (pdf-nametree-ref 
+	     (pdf-dref (pdf-dref (pdf-doc-catalog doc) '/Names)
+		       '/Dests)
+	     dest))
+      (puthash dest out (pdf-doc-names doc)))))
+
+(defun pdf-arrayfind (proc array)
+  (catch 'break
+    (dotimes (i (pdf-arraylen array))
+      (if (funcall proc (pdf-aref array i))
+	  (throw 'break i)))))
+
+(defun string-between (s l u)
+  (or
+   (and (string< s u)
+	(not (string< s l)))
+   (string= s l)
+   (string= s u)))
+
+(defun pdf-nametree-ref (ntree name)
+  "Dereference a named destination."
+  (let (kids limits ubound lbound ix)
+    (catch 'break
+      (while t
+	(if (setq kids (pdf-dref ntree '/Kids))
+	    (if (setq ix
+		      (pdf-arrayfind
+		       (lambda (kid)
+			 (setq limits (pdf-dref kid '/Limits)
+			       lbound (pdf-aref limits 0)
+			       ubound (pdf-aref limits 1))
+			 (string-between name lbound ubound))
+		       kids))
+		(setq ntree (pdf-aref kids ix))
+	      (throw 'break nil))
+	  (setq kids (pdf-dref ntree '/Names))
+	  (setq ix (pdf-arrayfind (lambda (kid)
+				    (if (stringp kid)
+					(string= kid name)))
+				  kids))
+	  (throw 'break (pdf-aref kids (+ ix 1))))))))
